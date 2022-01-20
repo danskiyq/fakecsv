8c561cc279758ed683d80df3cdf1201bcc1a71af
DanilDanylchenko <danild@superplay.co>
2022-01-21 01:55:10 +0200

    csv

--- fakecsv/owner.py
+++ fakecsv/owner.py
@@ -130,7 +130,7 @@ def gen_fake_csv(owner, rows):
                  '8': fake.random_int, '9': fake.address, '10': fake.date}
     for schema in schemas:
         csv_objects = parse_instructions(schema.read_instructions)
-        filepath = os.path.join('media', str(owner))
+        filepath = os.path.join(settings.MEDIA_ROOT, str(owner))
         with open(os.path.join(filepath, f'{schema.name}.csv'), 'w+') as f:
             writer = csv.writer(f, delimiter=get_separator(schema.separator))
             writer.writerow([i[0] for i in csv_objects])
--- planeks/settings.py
+++ planeks/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -139,6 +140,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
