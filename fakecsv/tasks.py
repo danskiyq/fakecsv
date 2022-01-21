49c33700f0284c17d5edb89db781e7db16cff64f
DanilDanylchenko <danild@superplay.co>
2022-01-21 05:08:43 +0200

    no celery

--- fakecsv/tasks.py
+++ fakecsv/tasks.py
@@ -1,8 +1,8 @@
-# from planeks.celery import app
-# from celery_progress.backend import ProgressRecorder
-# from .owner import gen_fake_csv
-#
-#
-# @app.task(bind=True)
-# def generate_fake_data(self, owner, rows):
-#     gen_fake_csv(owner, rows)
+from planeks.celery import app
+from celery_progress.backend import ProgressRecorder
+from .owner import gen_fake_csv
+
+
+@app.task(bind=True)
+def generate_fake_data(self, owner, rows):
+    gen_fake_csv(owner, rows)
