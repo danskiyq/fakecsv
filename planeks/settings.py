4597fa85f3a01ca0c4253990044b53f312fcfc71
DanilDanylchenko <danild@superplay.co>
2022-01-21 03:52:16 +0200

    csv

--- planeks/settings.py
+++ planeks/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
